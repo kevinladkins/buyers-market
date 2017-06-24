9fe9ea0a94a415cf2e3acc0af30871fe77795d76
github username <kevinladkins@gmail.com>
2017-06-24 13:43:55 -0400

    Build initial site header

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Jumbotron} from 'react-bootstrap'
 import './App.css';
+
+const storeHead = "Buyer's Market"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Jumbotron>
+          <h1>{storeHead}</h1>
+          <p>Connecting buyers with sellers</p>
+        </Jumbotron>
+
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
       </div>
     );
   }
